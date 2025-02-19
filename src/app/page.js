3229131f48b3505e2e61220e7f70ed01cf01360d
akhil soigama <akhilsoigama@gmail.com>
2025-02-19 10:32:11 +0530

    adds

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,10 @@
-'use client'
 import Hero from "./components/Hero";
 import StarBackground from "./components/StarBackground";
-import StarryBackground from "./components/StarBackground";
 
-import { useState, useEffect } from 'react';
+
 export default function Home() {
   return (
     <div className="w-full h-screen no-scrollbar ">
-      {/* <Sphere/> */}
-      {/* <StarryBackground/> */}
-     
       <Hero />
       <StarBackground/>
     </div>
